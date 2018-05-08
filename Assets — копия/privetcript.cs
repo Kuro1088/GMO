9a89f38fca25c61a61cee0cea88f50d6ff1764cb
Kuro1088 <39085311+Kuro1088@users.noreply.github.com>
2018-05-08 13:14:10 +0300

    Add files via upload

---
+++ Assets — копия/privetcript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class privetcript : MonoBehaviour {
+
+private Rigidbody = rb
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+
+        Vector3 dir = new Vector3(n,v,o);
+
+        transform.position += dir;
+
+
+
+
